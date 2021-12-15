996a095a94aeef28033012ff35af3b2298aef7b8
Vitória Ferreira <vitoriasouzaf@hotmail.com>
2021-12-15 20:11:17 -0300

    adicionado tela de login e media queries

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const cadastro = document.querySelector('.cadastro')
+const loginBtn = document.querySelector('.login-btn')
+const login = document.querySelector('.login')
+const cadastroBtn = document.querySelector('.cadastro-btn')
+
+loginBtn.onclick = () => {
+    login.style.display = 'block'
+    cadastro.style.display = 'none'
+}
+
+cadastroBtn.onclick = () => {
+    cadastro.style.display = 'block'
+    login.style.display = 'none'
+}
+
+
 const slides = document.querySelectorAll('.slides__photo')
 let currentPosition = 0
 
